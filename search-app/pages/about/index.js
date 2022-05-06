c1778873cb2189bd630c17700d5479f618b2e210
Cletus Kingdom <cletuskingdom@gmail.com>
2022-05-06 02:11:19 +0100

    updated the about page

---
+++ search-app/pages/about/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function index() {
+	return <div>Welcome to the About Page</div>;
+}
--- search-app/pages/index.js
+++ search-app/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
 						</div>
 						<div className="ml-3 inline-flex rounded-md shadow">
 							<a
-								href="#"
+								href="about"
 								className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
 							>
 								Learn more
