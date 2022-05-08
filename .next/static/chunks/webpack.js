c059375ca4186d1dcf68939c41cfabc0cfbba540
Cletus Kingdom <cletuskingdom@gmail.com>
2022-05-08 19:35:38 +0100

    Updated the homepage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "66311897a6cb036f"; }
+/******/ 		__webpack_require__.h = function() { return "a739bb1a969459fb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
